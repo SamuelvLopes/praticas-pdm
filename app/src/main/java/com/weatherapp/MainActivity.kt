5c79cf80db52ad10ccf32726ae740dff6c1d2b5d
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-08-29 18:02:17 -0300

    PRÁTICA 4 – PARTE 2

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
@@ -18,6 +22,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.weatherapp.model.MainViewModel
 import com.weatherapp.ui.components.BottomNavBar
@@ -27,7 +33,12 @@ import com.weatherapp.ui.nav.MainNavHost
 import com.weatherapp.ui.pages.HomePage
 import com.weatherapp.ui.theme.WeatherAppTheme
 
+import androidx.navigation.NavDestination.Companion.hasRoute
+import com.weatherapp.ui.nav.Route
+
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("RestrictedApi")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +47,11 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val viewModel : MainViewModel by viewModels()
             var showDialog by remember { mutableStateOf(false) }
+            val currentRoute = navController.currentBackStackEntryAsState()
+            val showButton = currentRoute.value?.destination?.hasRoute(Route.List::class) == true
+            val launcher = rememberLauncherForActivityResult(contract =
+                ActivityResultContracts.RequestPermission(), onResult = {} )
+
 
             WeatherAppTheme {
                 if (showDialog) CityDialog(
@@ -76,13 +92,16 @@ class MainActivity : ComponentActivity() {
                     },
 
                     floatingActionButton = {
-
-                        FloatingActionButton(onClick = {showDialog = true }) {
-                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        if (showButton) {
+                            FloatingActionButton(onClick = { showDialog = true }) {
+                                Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                            }
                         }
                     }
+
                 ) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
+                        launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
                         MainNavHost(navController = navController, viewModel = viewModel)
                     }
                 }
@@ -90,6 +109,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+private fun NavDestination?.hasRoute(string: String) {}
 
 @Preview(showBackground = true)
 @Composable
